210b7d940d13f98751d6b57e5ae1a9824fa2c5a8
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-08-06 11:05:46 +0200

    Updated the styles of the calculator

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -11,8 +11,6 @@ const Calculator = () => {
     const handlePositiveNegative = () => {
         setInitialVal(prevNum => (+prevNum * (-1)).toString())
     }
-    
-    
 
     const handleClick = (number) => {    
         if(initialVal === "0") {
@@ -37,9 +35,6 @@ const Calculator = () => {
         setInitialVal("0")
     }
 
-
-
-
     const handleResults = () => {
         switch (operators) {
             case "+":
@@ -57,44 +52,46 @@ const Calculator = () => {
             case "%":
                 setInitialVal(prevVal => +firstOperand % (+prevVal))
                 break
+
+            case ".":
+                setInitialVal(prevVal => firstOperand + "." + prevVal)
+                break
         }
     }
     
     return (
 
-        <div className='w-2/5 m-auto'>
-            <h1 className='text-center  bg-blue-500 py-3'>Calculator Application</h1>
+        <div className='w-1/3 m-auto items-center'>
+            <h1 className='text-center  bg-gray-500 py-3 text-3xl text-white font-bold'>Calculator Application</h1>
         
-                <div className=' flex'>
-                    <span className='py-5 text-4xl block'>{initialVal}</span>
-                </div>
+            <div className=' flex'>
+                <span className='py-5 text-4xl block'>{initialVal}</span>
+            </div>
 
             <div>
-                <div className='grid grid-cols-4'>
-                    <span className='bg-blue-300 border flex justify-center py-9 border-black' onClick={deleteInputs}>AC</span>
-                    <span className='bg-blue-300 border flex justify-center py-9 border-black' onClick={handlePositiveNegative}>+/-</span>
-                    <span className='bg-blue-300 border flex justify-center py-9 border-black' onClick={() => handleOperatorClick("%")}>%</span>
-                    <span className='bg-yellow-600 border flex justify-center py-9 border-black' onClick={() => handleOperatorClick("/")}>/</span>
+                <div className='grid grid-cols-4 gap-2'>
+                    <span className=' border flex justify-center py-5 border-black' onClick={deleteInputs}>AC</span>
+                    <span className=' border flex justify-center py-5 border-black' onClick={handlePositiveNegative}>+/-</span>
+                    <span className=' border flex justify-center py-5 border-black' onClick={() => handleOperatorClick("%")}>%</span>
+                    <div className='grid row-span-4 gap-2'>
+
+                        <span className='bg-yellow-600 border flex justify-center py-5 border-black row-span-2' onClick={() => handleOperatorClick("/")}>/</span>
+                        <span className='bg-yellow-600 border flex justify-center py-5 border-black ' onClick={() => handleOperatorClick("*")}>*</span>
+                        <span className='bg-yellow-600 border flex justify-center py-5 border-black ' onClick={() => handleOperatorClick("+")}>+</span> 
+                        <span className='bg-yellow-600 border flex justify-center py-5 border-black row-span-2' onClick={() => handleOperatorClick("-")}>-</span>
+                    
+                    </div>
                     {numbers.map((num, index) => (
+                        <span key={index} className={`border flex justify-center py-4 border-black ${num == 0 ? "col-span-2" : num}`} onClick={() => handleClick(num)}>{num}</span>
                         
-                    <span key={index} className='bg-blue-300 border flex justify-center py-9 border-black' onClick={() => handleClick(num)}>{num}</span>
                     ))}
                     
-                    <span className='bg-yellow-600 border flex justify-center py-9 border-black' onClick={() => handleOperatorClick("*")}>*</span>
-                    
-                    <span className='bg-yellow-600 border flex justify-center py-9 border-black' onClick={() => handleOperatorClick("-")}>-</span>
-                    <span className='bg-yellow-600 border flex justify-center py-9 border-black' onClick={() => handleOperatorClick("+")}>+</span> 
-                    <span className='bg-blue-300 border flex justify-center py-9 border-black'>.</span>
-                    <span className='bg-blue-300 border flex justify-center py-9 border-black' onClick={handleResults}>=</span>
+                    <span className=' border flex justify-center py-5 border-black' onClick={() => handleOperatorClick(".")}>.</span>
+                    <span className=' border flex justify-center py-5 border-black' onClick={handleResults}>=</span>
                 </div>
             </div>
         </div>
-      )
-
-
-    
-
-  
+      )  
 }
 
 export default Calculator
