8d340d4a1bfbcf90f5d014293f8b3424a33478ea
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-10-14 09:18:31 +0200

    Remove unecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import './App.css'
-import Calculator from './Calculator'
+import Calculator from "./Calculator";
 
 function App() {
-
-  /* const [values, setValues] = useState(0) */
-    /* const numbers = [1,2,3,4,5,6,7,8,9,0] */
-
-/*     const handleClickButtons = () => {
-        
-    }
- */
-  /*   let result = numbers.map((item, index) => 
-    
-        < Calculator value={item}/>
-    
-    )
- */
-  return (
-    <>
-    
-      < Calculator />
-    </>
-  )
+  return <Calculator />;
 }
 
-export default App
+export default App;
